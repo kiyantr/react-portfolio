1145c6e97f2994786950d0cdb4a9b84f34ef29c0
Kiet Tran <kiet.tran@dfogpc.com>
2025-02-05 13:44:51 +0700

    add avt

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,109 @@
-import { Container, Row, Col } from "react-bootstrap";
-import "./App.css";
-import { MdEmail } from "react-icons/md";
-import { FaBirthdayCake, FaPhoneAlt, FaLinkedin } from "react-icons/fa";
-import { SiGooglestreetview } from "react-icons/si";
-import certificate from "./certificate.jpg";
-import Projects from "./resume/projects";
-import PetProjects from "./resume/pet-projects";
-import { SiLeetcode } from "react-icons/si";
-import { GrContactInfo } from "react-icons/gr";
-import WorkExperience from "./resume/work-experience";
+import { Container, Row, Col } from 'react-bootstrap';
+import './App.css';
+import { MdEmail } from 'react-icons/md';
+import { FaBirthdayCake, FaPhoneAlt, FaLinkedin } from 'react-icons/fa';
+import { SiGooglestreetview } from 'react-icons/si';
+import certificate from './certificate.jpg';
+import Projects from './resume/projects';
+import PetProjects from './resume/pet-projects';
+import { SiLeetcode } from 'react-icons/si';
+import { GrContactInfo } from 'react-icons/gr';
+import WorkExperience from './resume/work-experience';
+import avatar from './resume/avatar.jpg';
 
 function App() {
-  const dateWork = Date.parse("2018-08-20");
+  const dateWork = Date.parse('2018-08-20');
 
   const experienceYear = Math.floor(
     (Date.now() - dateWork) / (1000 * 60 * 60 * 24 * 365)
   );
   return (
-    <div className="App">
+    <div className='App'>
       <Container>
         <Row>
-        <div className="name">TRAN VU KIET</div>
-        <div className="position">FULLSTACK DEVELOPER</div>
+          
           <Col lg={3} xs={12} style={{ paddingRight: 0 }}>
             {/* <Row className="justify-content-md-center">
               <div
                 style={{ background: "white", width: 210, height: 115 }}
-              ></div>
+              >
+              </div>
+                <img src={avatar} />
             </Row> */}
-            <Row className="justify-content-md-center">
+            <Row className='' style={{ paddingLeft: 15 }}>
+              <img src={avatar} alt='Kiet Tran' className='avt' />
+            </Row>
+            <Row className='justify-content-md-center'>
               {/* <Col md={12} xs={{ span: 6, offset: 3 }}> */}
               <Col md={12} xs={12}>
                 <Row>
-                  <Col sm={1} className="p0" xs={1}>
+                  <Col sm={1} className='p0' xs={1}>
                     <MdEmail />
                   </Col>
-                  <Col md={11} xs={11} className="p0">
-                    <a href="mailto:tranvukietk15@gmail.com">
-                      tranvukietk15@gmail.com
+                  <Col md={11} xs={11} className='p0'>
+                    <a href='mailto:kiettranv@gmail.com'>
+                      kiettranv@gmail.com
                     </a>
                   </Col>
                 </Row>
                 <Row>
-                  <Col className="p0" xs={1}>
+                  <Col className='p0' xs={1}>
                     <FaBirthdayCake />
                   </Col>
-                  <Col md={11} xs={11} className="p0">
+                  <Col md={11} xs={11} className='p0'>
                     Nov 1996
                   </Col>
                 </Row>
                 <Row>
-                  <Col className="p0" xs={1}>
+                  <Col className='p0' xs={1}>
                     <FaPhoneAlt />
                   </Col>
-                  <Col md={11} xs={11} className="p0">
-                    <a href="tel:+84949845485">+84 949 845 485</a>
+                  <Col md={11} xs={11} className='p0'>
+                    <a href='tel:+84949845485'>+84 949 845 485</a>
                   </Col>
                 </Row>
                 <Row>
-                  <Col xs={1} className="p0">
+                  <Col xs={1} className='p0'>
                     <SiGooglestreetview />
                   </Col>
-                  <Col md={11} xs={11} className="p0">
+                  <Col md={11} xs={11} className='p0'>
                     District 1, Ho Chi Minh City, VietNam
                   </Col>
                 </Row>
                 <Row>
-                  <Col xs={1} className="p0">
+                  <Col xs={1} className='p0'>
                     <GrContactInfo />
                   </Col>
-                  <Col md={11} xs={11} className="p0">
+                  <Col md={11} xs={11} className='p0'>
                     <a
-                      href="https://resume.kiettranv.uk"
-                      title="CV Tran Vu Kiet"
+                      href='https://kiettran.tramcrypto.com'
+                      title='CV Tran Vu Kiet'
                     >
-                      resume.kiettranv.uk
+                      kiettran.tramcrypto.com
                     </a>
                   </Col>
                 </Row>
                 <Row>
-                  <Col xs={1} className="p0">
+                  <Col xs={1} className='p0'>
                     <FaLinkedin />
                   </Col>
-                  <Col md={11} xs={11} className="p0">
+                  <Col md={11} xs={11} className='p0'>
                     <a
-                      href="https://www.linkedin.com/in/kiet-tran-555845129/"
-                      title="linkedin tranvukiet"
+                      href='https://www.linkedin.com/in/kiet-tran-555845129/'
+                      title='linkedin tranvukiet'
                     >
                       linkedin.com/in/kiet-tran-555845129
                     </a>
                   </Col>
                 </Row>
                 <Row>
-                  <Col xs={1} className="p0">
+                  <Col xs={1} className='p0'>
                     <SiLeetcode />
                   </Col>
-                  <Col md={11} xs={11} className="p0">
+                  <Col md={11} xs={11} className='p0'>
                     <a
-                      href="https://leetcode.com/tranvukietk15/"
-                      title="Leetcode tranvukiet"
+                      href='https://leetcode.com/tranvukietk15/'
+                      title='Leetcode tranvukiet'
                     >
                       leetcode.com/tranvukietk15
                     </a>
@@ -108,29 +113,33 @@ function App() {
             </Row>
           </Col>
           <Col lg={9} xs={12} style={{ paddingRight: 0, paddingLeft: 20 }}>
-            
-            <div className="summary">
-              Highly experienced full-stack developer with over{" "}
-              {experienceYear + " "}
+          <div className='name'>TRAN VU KIET</div>
+          <div className='position'>Fullstack Developer</div>
+            <div className='summary'>
+              Highly experienced full-stack developer with over{' '}
+              {experienceYear + ' '}
               years of experience building and maintaining web applications
-              using NodeJS and .NET Core. Proven ability to design, develop, and
-              implement high-performance, scalable, and maintainable web
-              applications. Expertise in all aspects of the fullstack
-              development process, including front-end development, back-end
-              development, and DevOps. Strong understanding of NodeJS and .NET Core,
-              ReactJS, TypeScript, and related web technologies.
+              using .NET Core, NodeJS and ReactJS. Proven ability to design,
+              develop, and implement high-performance, scalable, and
+              maintainable web applications. Expertise in all aspects of the
+              fullstack development process, including front-end development,
+              back-end development, and DevOps. Strong understanding of NodeJS
+              and .NET Core, ReactJS, TypeScript, and related web technologies.
               Experience with RESTful APIs, microservices architecture, and
               cloud platforms such as AWS or Azure. Excellent problem-solving
               and analytical skills. Ability to work independently and as part
-              of a team. Strong communication skills and hard working.
-              I am willing to learn new programming languages, looking for long term commitment.
+              of a team. Strong communication skills and hard working. I am
+              willing to learn new programming languages, looking for long term
+              commitment.
             </div>
             {/* <div>
               Career path next 5 years: I want to become a technical specialist,
               aiming for a tech lead or solution architect position.
             </div> */}
-            <div className="summary">Favorites: read books, algorithms, math, AI.</div>
-            <div className="summary">Available for work immediately.</div>
+            <div className='summary'>
+              Favorites: read books, algorithms, math, AI.
+            </div>
+            <div className='summary'>Available for work immediately.</div>
             {/* <div>
               My goals: I am passionate about the crypto currency
 							market and I see the huge potential of Web3. That is
@@ -143,11 +152,11 @@ function App() {
           </Col>
         </Row>
         <Row>
-          <div className="title">Skills</div>
+          <div className='title'>Skills</div>
         </Row>
         {Skills()}
         <Row>
-          <div className="title">Work experience</div>
+          <div className='title'>Work experience</div>
         </Row>
         <WorkExperience />
         {/* <Row>
@@ -155,21 +164,21 @@ function App() {
 					<Col>2 of 3</Col>
 				</Row> */}
         <Row>
-          <div className="title">Projects</div>
+          <div className='title'>Projects</div>
         </Row>
         <Projects />
         <Row>
-          <div className="title">Pet Projects</div>
+          <div className='title'>Pet Projects</div>
         </Row>
         <PetProjects />
         <Row>
-          <div className="title">Education</div>
+          <div className='title'>Education</div>
         </Row>
-        <Row className="last">
+        <Row className='last'>
           <Col lg={4} xs={12}>
             <Row>
               <Col xs={3}>School</Col>
-              <Col xs={9} className="bold">
+              <Col xs={9} className='bold'>
                 MEKONG UNIVERSITY
               </Col>
             </Row>
@@ -194,75 +203,75 @@ function App() {
   function Skills() {
     const leftSkills = [
       {
-        type: "Backend",
-        skills: ["TypeScript NodeJS, NestJS, NextJS", "C# .NET core"],
+        type: 'Backend',
+        skills: ['C# .NET core', 'TypeScript NodeJS, NextJS'],
         // certificate: certificate
       },
       {
-        type: "FrontEnd",
-        skills: ["ReactJS, Redux, Bootstrap, Tailwindcss"],
+        type: 'FrontEnd',
+        skills: ['ReactJS, Redux, Bootstrap, Tailwindcss'],
       },
       {
-        type: "Database",
-        skills: ["MSSQL, MySQL, MongoDb, Redis, Postgres, DynamoDb"],
+        type: 'Database',
+        skills: ['MSSQL, MySQL, MongoDb, Redis, Postgres, DynamoDb'],
       },
       {
-        type: "Framework",
-        skills: [
-          "NestJS, NextJS v14, NextAuth v4",
-          "Express, Fastify",
-          ".NET core version: 3.1, 5, 6, 7, 8",
-        ],
+        type: 'Framework',
+        skills: ['.NET core version: 3.1, 5, 6, 7, 8', 'Express, Fastify'],
+      },
+      {
+        type: 'Unit Testing',
+        skills: ['NUnit, XUnit, Integration Test'],
       },
     ];
     const rightSkills = [
       {
-        type: "API",
-        skills: ["REST API, GRAPHQL"],
+        type: 'API',
+        skills: ['REST API, GRAPHQL'],
       },
       {
-        type: "Message broker",
-        skills: ["Azure service bus, SNS - SQS, RabbitMQ"],
+        type: 'Message broker',
+        skills: ['Azure service bus, AWS SNS - SQS, RabbitMQ'],
       },
       {
-        type: "CI/CD",
-        skills: ["Azure pipeline, Jenkins, Github Action"],
+        type: 'CI/CD',
+        skills: ['Azure pipeline, Jenkins, Git Action'],
       },
       {
-        type: "Hosting",
-        skills: ["Cloudflare"],
+        type: 'Hosting',
+        skills: ['Cloudflare'],
       },
       {
-        type: "VPS",
+        type: 'VPS',
         skills: [
-          "EC2, Azure AppService, Azure Function, Contabo, Physical server",
+          'EC2, Azure AppService, Azure Function, Contabo, Physical server',
         ],
       },
     ];
     const generateSkills = (skills: Array<any>) =>
       skills.map((item, inx) => (
         <Row key={`${item.type}_${inx}`}>
-          <Col md={4} sm={4} xs={5} className="bold">
+          <Col md={4} sm={4} xs={5} className='bold'>
             {item.type}
           </Col>
-          <Col md={8} sm={8} xs={7} className={item.className ?? ""}>
+          <Col md={8} sm={8} xs={7} className={item.className ?? ''}>
             {item.skills.map((skill: any, i: number) => (
               <Row key={`skill_${i}`}>
                 <Col md={12}>{skill}</Col>
               </Row>
             ))}
             {item.certificate && (
-              <div className="review-container">
+              <div className='review-container'>
                 <a
-                  target="blank"
-                  href="https://www.udemy.com/certificate/UC-107775f7-d760-4c79-997f-93695e16ed95/"
-                  className="review-link"
+                  target='blank'
+                  href='https://www.udemy.com/certificate/UC-107775f7-d760-4c79-997f-93695e16ed95/'
+                  className='review-link'
                 >
                   Solidity Certificate
                   <img
                     src={certificate}
-                    alt="Review Image"
-                    className="review-image"
+                    alt='Review Image'
+                    className='review-image'
                   />
                 </a>
               </div>
--- src/resume/projects.tsx
+++ src/resume/projects.tsx
@@ -163,7 +163,7 @@ CxWow: A customer service management platform that integrates AI (ChatGPT) for a
 			team members, build core ReactJS, create docker and
 			deploy web to VPS centos 7, setup nginx and jenkins.`,
       technologies: [
-        `Typescript, .Net 6, SQL, ReactJS, S3, Onion Architeture, Docker, nginx, jenkins.`,
+        `Typescript, .Net 6, SQL, ReactJS, S3, Onion Architeture, Docker, nginx, jenkins, XUnit.`,
       ],
       contributed: [
         `Init project and apply clean architecture.`,
@@ -235,7 +235,7 @@ CxWow: A customer service management platform that integrates AI (ChatGPT) for a
         'design pattern: Singleton, Factory, CQRS, Builder, Mediator',
       ],
       contributed: [
-        `Write Unit Test.`,
+        `Write Unit Test with NUnit.`,
         `Implement task.`,
         `Implement new API with GraphQL.`,
         `Involve build table table per hierarchy and calculate point.`,
